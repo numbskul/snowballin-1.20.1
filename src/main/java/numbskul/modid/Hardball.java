bf1dc57a16960bfad7a6a792545f202f345dfb81
matt1 <matterider@gmail.com>
2023-12-29 19:19:06 -0800

    rockball is done, hopefully- need to test with twigs next is powdered snow?

--- src/main/java/numbskul/modid/Hardball.java
+++ src/main/java/numbskul/modid/Hardball.java
@@ -2,6 +2,9 @@ package numbskul.modid;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -16,14 +19,15 @@ public class Hardball implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("hardball");
 
 	// item(s)!
-	public static final RockballItem ROCK_BALL = Registry.register(Registries.ITEM, new Identifier("hardball", "rockball"), new RockballItem(new FabricItemSettings()));
-
+	public static final RockballItem ROCK_BALL = Registry.register(Registries.ITEM, new Identifier("hardball", "rockball"), new RockballItem(new FabricItemSettings().maxCount(16)));
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		LOGGER.info("hardball (snowball mod) says hello world!");
-
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+			content.addAfter(Items.SNOWBALL, ROCK_BALL);
+		});
 	}
 }
